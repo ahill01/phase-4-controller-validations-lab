5831e6d765156ac87834ae06536d6d32bf3923c8
Allison <amh03160@gmail.com>
2022-05-02 10:31:04 -0500

    commit tree 9f93d5f3149b4e266109d1858967aa55595f1a32

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_message
   def show
     post = Post.find(params[:id])
     
@@ -8,9 +8,7 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
   end
 
@@ -20,4 +18,9 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  private
+
+  def unprocessable_entity_message(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validate :valid_category
+
+    private
+    def valid_category
+        valid_categories = ["Fiction","Non-Fiction"]
+        if(!valid_categories.include?(category))
+            errors.add(:category, "is not included in the list")
+        end
+    end
 end
